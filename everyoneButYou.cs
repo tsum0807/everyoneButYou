f49d38eeaaf03f16ecef0bdd43431049032f699d
beneb <sumetvarapaben@gmail.com>
2020-05-03 22:01:11 +1000

    Project initialisation

---
+++ everyoneButYou.cs
@@ -0,0 +1,17 @@
+﻿using RoR2;
+using BepInEx;
+
+
+namespace beneb
+{
+    [BepInDependency("com.bepis.r2api")]
+    //Change these
+    [BepInPlugin("everyoneButYou", "everyoneButYou", "1.0.0")]
+    public class everyoneButYou : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            Chat.AddMessage("Loaded everyoneButYou");
+        }
+    }
+}
